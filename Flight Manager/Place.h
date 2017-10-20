9200ffeba1275ed712323522176b0fc7925cbe3f
Danyalo <danylolab@gmail.com>
2017-10-20 09:05:05 +0300

    Fix
    Updated Place.h

--- Flight Manager/Place.h
+++ Flight Manager/Place.h
@@ -1,11 +1,14 @@
 #pragma once
+
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class Place{
 protected:
 	string Place_number;
-	bool availability;
+	bool availability;			// 0 - empty seat
 public:
 	Place();
 	Place(string _Place_number, bool _availability);
