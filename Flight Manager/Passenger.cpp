7da4e7a878ca121c07095ac6c081cb037c2f0004
Max <maxym1998@gmail.com>
2017-10-22 00:11:28 +0300

    Not finished Passanger
    To be continued

--- Flight Manager/Passenger.cpp
+++ Flight Manager/Passenger.cpp
@@ -1,38 +1,57 @@
 #include <iostream>
 #include "Passenger.h"
 using namespace std;
-Passenger::Passenger() :Passenger_Name("incognito"), Passenger_Surname("incognito"), Passenger_Gender("incognito"), Passenger_Nationality("incognito"), Passenger_Passport_No(0), Passenger_Date_of_Birth(0), Passenger_Phone_number(0)
-{}
-Passenger::Passenger(string _Passenger_Name, string _Passenger_Surname, string _Passenger_Gender, string _Passenger_Nationality, int _Passenger_Passport_No, int _Passenger_Date_of_Birth, int _Passenger_Phone_number) : Passenger_Name(_Passenger_Name), Passenger_Surname(_Passenger_Surname), Passenger_Gender(_Passenger_Gender), Passenger_Nationality(_Passenger_Nationality), Passenger_Passport_No(_Passenger_Passport_No), Passenger_Date_of_Birth(_Passenger_Date_of_Birth), Passenger_Phone_number(_Passenger_Phone_number)
-{}
-string Passenger::getPassenger_Name()const
+Passenger::Passenger() :name(NULL), surname(NULL),sex(NULL),passport_No(NULL),date_of_Birth(NULL),phonenumber(NULL)
 {
-	return Passenger_Name;
+
 }
-void Passenger::setPassenger_Name(string _Name)
+
+Passenger::Passenger(string _name, string _surname, string _sex, string _nationality, string _passport_No, string _date_of_Birth, string _phonenumber) 
 {
-	Passenger_Name.assign(_Name);
+	name.assign(_name);
+	surname.assign(_surname);
+	sex.assign(_sex);
+	nationality.assign(_nationality);
+	passport_No.assign(_passport_No);
+	date_of_Birth.assign(_date_of_Birth);
+	phonenumber.assign(_phonenumber);
 }
+
+string Passenger::getName()const
+{
+	return name;
+}
+
+void Passenger::setName(string _Name)
+{
+	name.assign(_Name);
+}
+
 string Passenger::getPassenger_Surname()const
 {
 	return Passenger_Surname;
 }
+
 void Passenger::setPassenger_Surname(string _Surname)
 {
 	Passenger_Surname.assign(_Surname);
 }
+
 string Passenger::getPassenger_Gender()const
 {
 	return Passenger_Gender;
 }
+
 void Passenger::setPassenger_Gender(string _Gender)
 {
 	Passenger_Surname.assign(_Gender);
 }
+
 string Passenger::getPassenger_Nationality()const
 {
 	return Passenger_Nationality;
 }
+
 void Passenger::setPassenger_Nationality(string _Nationality)
 {
 	Passenger_Surname.assign(_Nationality);
@@ -41,6 +60,7 @@ int Passenger::getPassenger_Passport_No()const
 {
 	return Passenger_Passport_No;
 }
+
 void Passenger::setPassenger_Passport_No(int _Passport_No)
 {
 	Passenger_Passport_No = _Passport_No;
@@ -49,18 +69,22 @@ int Passenger::getPassenger_Date_of_Birth()const
 {
 	return Passenger_Date_of_Birth;
 }
+
 void Passenger::setPassenger_Date_of_Birth(int _Date_of_Birth)
 {
 	Passenger_Date_of_Birth = _Date_of_Birth;
 }
+
 int Passenger::getPassenger_Phone_number()const
 {
 	return Passenger_Phone_number;
 }
+
 void Passenger::setPassenger_Phone_number(int _Phone_number)
 {
 	Passenger_Phone_number = _Phone_number;
 }
+
 Passenger::~Passenger()
 {
 	Passenger_Name = " ";
